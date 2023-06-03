e977c58443d2519e6afcd0ed3f30077e50dd8f90
Nguyễn Tiến Nhất <nhatnthe153132@fpt.edu.vn>
2023-06-03 21:22:43 +0700

    update sound subtract coin when buy and sell tower

--- Assets/Script/Manager/ScoreManager.cs
+++ Assets/Script/Manager/ScoreManager.cs
@@ -15,7 +15,7 @@ public class ScoreManager : MonoBehaviour
     {
         currentScore += enemyPoint;
         StatusScore.currentScore = currentScore;
-      //  Debug.Log("Current Score: "+ currentScore );
+        //Debug.Log("Current Score: "+ currentScore );
     }
 
     public static void SubtractScoreUpgradeTower(int upgradeCost)
@@ -25,6 +25,8 @@ public class ScoreManager : MonoBehaviour
             currentScore -= upgradeCost;
             StatusScore.currentScore = currentScore;
          //   Debug.Log("Current Score: "+ currentScore );
+            AudioManager.Play(AudioClipName.SubtractCoin);
+
         }
         else
         {
@@ -37,5 +39,6 @@ public class ScoreManager : MonoBehaviour
         currentScore += towerPrice;
         StatusScore.currentScore = currentScore;
        // Debug.Log("Current Score: "+ currentScore );
+        AudioManager.Play(AudioClipName.SubtractCoin);
     }
 }
